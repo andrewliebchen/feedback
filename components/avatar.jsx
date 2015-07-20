39797a76bdc81178de049de1edf3c6fe55055bcc
andrewliebchen <andrewliebchen@gmail.com>
2015-07-19 22:59:59 -0700

    Basic employee register form

--- components/avatar.jsx
+++ components/avatar.jsx
@@ -9,9 +9,11 @@ Avatar = React.createClass({
     var profile = this.props.employee.profile;
     return (
       <a href={`/employees/${this.props.employee._id}`} className="avatar media">
-        <div className="media-left">
-          <img src={profile.picture.thumbnail} className="img-rounded" width="24"/>
-        </div>
+        {this.props.thumbnail ?
+          <div className="media-left">
+            <img src={profile.picture.thumbnail} className="img-rounded" width="24"/>
+          </div>
+        : null}
         <div className="media-body">
           {`${_.capitalize(profile.name.first)} ${_.capitalize(profile.name.last)}`}
         </div>
---
+++ components/registration.jsx
@@ -0,0 +1,106 @@
+/**
+ * @jsx React.DOM
+ */
+
+var _ = lodash;
+
+Registration = ReactMeteor.createClass({
+  getMeteorState() {
+    return {
+      organization: Organizations.findOne()
+    };
+  },
+
+  handleCreateEmployee() {
+    Meteor.call('registerEmployee', {
+      email: React.findDOMNode(this.refs.email).value,
+      password: React.findDOMNode(this.refs.password).value,
+      profile: {
+        organization: this.state.organization._id,
+        name: {
+          first: React.findDOMNode(this.refs.firstName).value,
+          last: React.findDOMNode(this.refs.lastName).value
+        }
+      }
+    }, function(error, success) {
+      if(success) {
+        FlowRouter.go('/login');
+      } else {
+        console.log(error);
+      }
+    });
+  },
+
+  render() {
+    return (
+      <div className="container">
+        <Header/>
+        <section className="panel panel-default">
+          <header className="panel-heading">
+            <h3 className="panel-title">Register for {this.state.organization.name}</h3>
+          </header>
+          <form>
+            <div className="panel-body">
+              <p>Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh.</p>
+              <div className="form-group">
+                <label>Your email</label>
+                <input type="email" className="form-control" ref="email" required/>
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" ref="password" required/>
+              </div>
+              <div className="form-group">
+                <label>First name</label>
+                <input type="type" className="form-control" ref="firstName"/>
+              </div>
+              <div className="form-group">
+                <label>Last name</label>
+                <input type="type" className="form-control" ref="lastName"/>
+              </div>
+            </div>
+            <footer className="panel-footer">
+              <button className="btn btn-primary" onClick={this.handleCreateEmployee}>Join organization</button>
+            </footer>
+          </form>
+        </section>
+      </div>
+    );
+  }
+});
+
+if(Meteor.isClient) {
+  FlowRouter.route('/register/:_id', {
+    subscriptions: function(params) {
+      this.register('registration', Meteor.subscribe('registration', params._id));
+    },
+
+    action: function() {
+      FlowRouter.subsReady('registration', function() {
+        React.render(<Registration/>, document.getElementById('yield'));
+      });
+    }
+  });
+}
+
+if(Meteor.isServer) {
+  Meteor.publish('registration', function(id) {
+    return Organizations.find({_id: id});
+  });
+
+  Meteor.methods({
+    'registerEmployee': function(employee) {
+      return Accounts.createUser({
+        email: employee.email,
+        password: employee.password,
+        profile: {
+          organization: employee.profile.organization,
+          name: {
+            first: employee.profile.name.first,
+            last: employee.profile.name.last
+          }
+        }
+      });
+    }
+  });
+}
