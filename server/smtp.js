33ec058d5ce0220625048837e6b280a0acfd4214
andrewliebchen <andrewliebchen@gmail.com>
2015-08-08 14:10:50 -0700

    Basic setup for invite emails

--- server/smtp.js
+++ server/smtp.js
@@ -2,7 +2,7 @@ Meteor.startup(function () {
   smtp = {
     username: Meteor.settings.sendgrid.username,
     password: Meteor.settings.sendgrid.password,
-    host:   Meteor.settings.sendgrid.host
+    host: Meteor.settings.sendgrid.host
   }
 
   process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.host);
