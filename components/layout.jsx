c9af75749879e8a166d36b2deb6bc864b6d57f9e
andrewliebchen <andrewliebchen@gmail.com>
2015-08-09 12:32:59 -0700

    Basic login page

--- components/layout.jsx
+++ components/layout.jsx
@@ -2,18 +2,28 @@
  * @jsx React.DOM
  */
 
+Header = React.createClass({
+  render() {
+    return (
+      <header className="header">
+        <div className="header__brand">
+          <a href="/admin"><strong>Feedback</strong></a>
+        </div>
+        {this.props.session ?
+          <div className="header__session">
+            <IncludeTemplate template={Template.loginButtons}/>
+          </div>
+        : null}
+      </header>
+    );
+  }
+});
+
 Layout = React.createClass({
   render() {
     return (
       <div className="container">
-        <header className="header">
-          <div className="header__brand">
-            <a href="/admin"><strong>Feedback</strong></a>
-          </div>
-            <div className="header__session">
-              <IncludeTemplate template={Template.loginButtons}/>
-            </div>
-        </header>
+        <Header session/>
         {this.props.content}
       </div>
     );
