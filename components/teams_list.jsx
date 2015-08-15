2174776e3aa1e897cc50895d286ac57a8605ce49
andrewliebchen <andrewliebchen@gmail.com>
2015-08-15 16:28:41 -0700

    Remove employee from team

--- components/teams_list.jsx
+++ components/teams_list.jsx
@@ -5,23 +5,42 @@
 const _ = lodash;
 
 const Team = React.createClass({
+  handleRemoveEmployee(employeeId) {
+    Meteor.call('removeEmployee', {
+      team: this.props.team._id,
+      employee: employeeId
+    });
+  },
+
   render() {
     return (
-      <li className="list-group-item">
-        <strong>{this.props.team.name}</strong>
+      <div className="panel panel-default">
+        <header className="panel-heading clearfix">
+          <h4 className="panel-title pull-left">{this.props.team.name}</h4>
+          <button className="btn btn-danger btn-xs pull-right">Delete team</button>
+        </header>
         {this.props.team.members.length > 0 ?
-          <ul className="list-group">
-            {this.props.team.members.map((member, i) => {
-              let employee = _.filter(this.props.employees, {_id: member});
-              return (
-                <li key={i} className="list-group-item">
-                  <Avatar employee={employee[0]}/>
-                </li>  
-              );
-            })}
-          </ul>
-        : null}
-      </li>
+          <table className="table">
+            <tbody>
+              {this.props.team.members.map((member, i) => {
+                let employee = _.filter(this.props.employees, {_id: member});
+                return (
+                  <tr key={i}>
+                    <td><Avatar employee={employee[0]}/></td>
+                    <td>
+                      <button
+                        className="btn btn-default btn-xs"
+                        onClick={this.handleRemoveEmployee.bind(null, employee[0]._id)}>
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        : <div className="panel-body">No members</div>}
+      </div>
     );
   }
 });
@@ -46,13 +65,23 @@ TeamsList = React.createClass({
           <h3 className="panel-title">Teams</h3>
         </header>
         <div className="panel-body">
-          <ul className="list-group">
-            {this.data.teams.map((team, i) => {
-              return <Team team={team} employees={this.data.employees} key={i}/>;
-            })}
-          </ul>
+          {this.data.teams.map((team, i) => {
+            return <Team team={team} employees={this.data.employees} key={i}/>;
+          })}
         </div>
       </section>
     );
   }
 });
+
+if(Meteor.isServer) {
+  Meteor.methods({
+    'removeEmployee': function(args) {
+      Teams.update(args.team, {
+        $pull: {
+          members: args.employee
+        }
+      });
+    }
+  });
+}
