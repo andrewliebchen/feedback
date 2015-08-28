a0663c1f8f11faec9c5f25d42d04637b42cb5fb4
andrewliebchen <andrewliebchen@gmail.com>
2015-08-27 17:36:52 -0700

    Stub in the 'side header'

--- components/layout.jsx
+++ components/layout.jsx
@@ -9,9 +9,7 @@ Header = React.createClass({
   render() {
     return (
       <header className="header">
-        <div className="header__brand">
-          <a href="/"><strong>Feedback</strong></a>
-        </div>
+        <a href="/" className="header__brand">F</a>
         {this.props.session ?
           <div className="header__session">
             <IncludeTemplate template={Template.loginButtons}/>
