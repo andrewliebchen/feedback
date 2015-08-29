dcdf489b6e2417730a63d6d8ed437d5eea0552a1
andrewliebchen <andrewliebchen@gmail.com>
2015-08-29 12:47:32 -0700

    Header actions and layout tweaks

--- components/header.jsx
+++ components/header.jsx
@@ -4,15 +4,42 @@
 
 const _ = lodash;
 
+const Session = React.createClass({
+  getInitialState() {
+    return {
+      menu: false
+    };
+  },
+
+  handleMenuToggle() {
+    this.setState({menu: !this.state.menu});
+  },
+
+  render() {
+    return (
+      <span>
+        <a className="header__session header__block" onClick={this.handleMenuToggle}>
+          <img src={Meteor.user().profile.imageSrc}/>
+        </a>
+        {this.state.menu ?
+          <div className="overlay">
+            <div className="overlay__toggle">close</div>
+            Profile
+            Sign out
+          </div>
+        : null}
+      </span>
+    );
+  }
+});
+
 Header = React.createClass({
   render() {
     return (
       <header className="header column_app">
-        <a href="/" className="header__brand">F</a>
+        <a href="/" className="header__brand header__block">F</a>
         {this.props.session ?
-          <div className="header__session">
-            <IncludeTemplate template={Template.loginButtons}/>
-          </div>
+          <Session/>
         : null}
       </header>
     );
